9b63c32efd29f28625ef489ad2c986a9bdd7f1b2
Ramzyjof <87891551+Ramzyjof@users.noreply.github.com>
2026-04-01 10:58:16 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import Problem from "./Components/Problem";
-import Authority from "./Components/Authority";
-import Services from "./Components/Services";
-import Pricing from "./Components/Pricing";
-import { CTA, Footer } from "./Components/CTA";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Problem from "./components/Problem";
+import Authority from "./components/Authority";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import { CTA, Footer } from "./components/CTA";
 
 export default function Page() {
   return (
@@ -17,13 +17,6 @@ export default function Page() {
       <Pricing />
       <CTA />
       <Footer />
-
-      <a
-        href="https://wa.me/6281212940797"
-        className="fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded-full"
-      >
-        WhatsApp
-      </a>
     </>
   );
 }
