4fc6a4cc0c63c639b6ff391a60fd5f2de57e69f7
Ramzyjof <ramzyjofvansah2000@gmail.com>
2026-04-06 13:37:50 +0700

    fix react import + client mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 const [pressed, setPressed] = useState(false);
 
